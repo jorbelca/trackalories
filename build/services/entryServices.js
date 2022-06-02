dd4fc814fc0d64a21b179a7d00f108d0c981ddea
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-06-02 13:10:54 +0200

    build v 1

---
+++ build/services/entryServices.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.modifyEntry = exports.deleteEntry = exports.addEntry = exports.getEntries = void 0;
+const getEntries = () => { return 'A'; };
+exports.getEntries = getEntries;
+const addEntry = () => {
+    return 'A';
+};
+exports.addEntry = addEntry;
+const deleteEntry = () => {
+    return 'A';
+};
+exports.deleteEntry = deleteEntry;
+const modifyEntry = () => { return 'A'; };
+exports.modifyEntry = modifyEntry;
